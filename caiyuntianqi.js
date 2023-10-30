119c5efdbdc5b48fa0a00c36886491b1f70643f4
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-10-30 21:08:46 +0800

    更新 caiyuntianqi.js

--- caiyuntianqi.js
+++ caiyuntianqi.js
@@ -1,6 +1,6 @@
 /*************************************
 
-项目名称：彩云天气-净化/部分SVIP
+项目名称：彩云天气
 下载地址：https://t.cn/A66d95hV
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
@@ -9,75 +9,48 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|one_key_login|third_party_login|activity|visitors|operation\/banners)|p\/v\d\/(vip_info|user_info)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
-^http:\/\/adx\.sogaha\.cn\/sdk\/ad\/get url reject-200
+# VIP信息
+^https?:\/\/(biz|wrapper|starplucker)\.(cyapi|caiyunapp)\.(cn|com)\/(.+\/(user\?app_name|activity\?app_name|visitors|operation\/banners)|p\/v\d\/(vip_info|user_info)) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
+# SVIP地图-48小时预报(方法来源:苍井灰灰)
+^https:\/\/wrapper\.cyapi\.cn\/v1\/(satellite|nafp\/origin_images) url script-request-header https://raw.githubusercontent.com/chxm1023/Rewrite/main/caiyuntianqi.js
 
 [mitm]
-hostname = *.cyapi.cn, *.caiyunapp.com, adx.sogaha.cn
+hostname = *.cyapi.cn, *.caiyunapp.com
 
 *************************************/
 
 
-var chxm1023 = JSON.parse($response.body);
+const chxm1024 = {};
+const chxm1023 = JSON.parse(typeof $response != "undefined" && $response.body || null);
 const url = $request.url;
-const adUrl = /https:\/\/(wrapper|starplucker)\.(cyapi\.cn|caiyunapp\.com)\/v\d\/(activity|banners)/;
+const adUrl = /(activity\?app_name|operation\/banners)/;
 const vipUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/p\/v\d\/vip_info/;
 const userUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/v\d\/user\?app_name/;
 const infoUrl = /https:\/\/biz\.(cyapi\.cn|caiyunapp\.com)\/p\/v\d\/user_info/;
 
-
-switch (true) {
+if (typeof $response == "undefined") {
+  chxm1024.headers = $request.headers;
+  chxm1024.headers['device-token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2ZXJzaW9uIjoxLCJ1c2VyX2lkIjoiNWY1YmZjNTdkMmM2ODkwMDE0ZTI2YmI4Iiwic3ZpcF9leHBpcmVkX2F0IjoxNzA1MzMxMTY2LjQxNjc3MSwidmlwX2V4cGlyZWRfYXQiOjB9.h_Cem89QarTXxVX9Z_Wt-Mak6ZHAjAJqgv3hEY6wpps';
+} else {
+  switch (true) {
     case adUrl.test(url):
-        chxm1023.activities = [];
-        chxm1023.data = [];
-        break;
+      chxm1023.status = "ok";
+      chxm1023.activities = [{"items":[{}]}];
+      chxm1023.data = [];
+      break;
     case vipUrl.test(url):
-        chxm1023 = {  ...chxm1023,  vip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  },  trial_svip: {    ...chxm1023.trial_svip,    "received_time" : "1666666666",    "expires_time" : "4092599349",    "is_recharge_vip" : true  },  svip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  }};
-        break;
+      chxm1023 = {  ...chxm1023,  vip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  },  trial_svip: {    ...chxm1023.trial_svip,    "received_time" : "1666666666",    "expires_time" : "4092599349",    "is_recharge_vip" : true  },  svip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  }};
+      break;
     case userUrl.test(url):
-        chxm1023.result = {
-            ...chxm1023.result,
-            is_vip: true,
-            vip_expired_at: 4092599349,
-            svip_given: 9999,
-            is_xy_vip: true,
-            xy_svip_expire: 4092599349,
-            wt: {
-              ...chxm1023.result.wt,
-              vip: {
-                ...chxm1023.result.wt.vip,
-                "expired_at" : 0,
-                "enabled" : true,
-                "svip_apple_expired_at" : 4092599349,
-                "is_auto_renewal" : true,
-                "svip_expired_at" : 4092599349
-              },
-              svip_given: 9999,
-            },
-            is_phone_verified: true,
-            vip_take_effect: 1,
-            is_primary: true,
-            xy_vip_expire: 4092599349,
-            svip_expired_at: 4092599349,
-            svip_take_effect: 1,
-            vip_type: "s",
-            phone_num: "13145200000",
-            name: "叮当猫の分享频道",
-            avatar: "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132",
-            caiyun: {
-              ...chxm1023.result.caiyun,
-              "username" : "叮当猫の分享频道",
-              "is_bound" : true
-            }
-          };
-        break;
+      chxm1023.result = { ...chxm1023.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 9999,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...chxm1023.result.wt,  vip: {  ...chxm1023.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 9999,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  phone_num: "13145200000",  name: "叮当猫の分享频道",  avatar: "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132",  bound_status: {  ...chxm1023.result.bound_status,  caiyun: {  ...chxm1023.result.bound_status.caiyun,  "username" : "叮当猫の分享频道",  "is_bound" : true}}};
+      break;
     case infoUrl.test(url):
-        chxm1023["reg_days"] = 99999;
-        chxm1023["name"] = "叮当猫の分享频道";
-        chxm1023["avatar"] = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132";
-        break;
-    default:
-        console.log("URL didn't match any cases");
+      chxm1023["reg_days"] = 99999;
+      chxm1023["name"] = "叮当猫の分享频道";
+      chxm1023["avatar"] = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132";
+      break;
+    }
+  chxm1024.body = JSON.stringify(chxm1023);
 }
 
-$done({ body: JSON.stringify(chxm1023)});
+$done(chxm1024);
