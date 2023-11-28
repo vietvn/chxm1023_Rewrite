3faebf8b3b933c98b7763f2e1a24fec70377547a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-11-28 21:53:27 +0800

    更新 nicegram.js

--- nicegram.js
+++ nicegram.js
@@ -1,7 +1,8 @@
 /*************************************
 
-项目名称：Nicegram
+项目名称：Nicegram(兼容新老旧版)
 下载地址：https://t.cn/A6ou0MCe
+更新日期：2023-11-28
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -9,15 +10,35 @@
 **************************************
 
 [rewrite_local]
-^https?:\/\/restore-access\.indream\.app\/restoreAccess url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
+^https?:\/\/(nicegram\.cloud\/api\/v\d\/user\/info|restore-access\.indream\.app\/restoreAccess) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/nicegram.js
 
 [mitm]
-hostname = restore-access.indream.app
+hostname = nicegram.cloud, restore-access.indream.app
 
 *************************************/
 
 
-const isQX = typeof $task != "undefined";
-const chxm1023 = {"data":{"premiumAccess": true}};
-console.log('已操作成功🎉\n叮当猫の分享频道: https://t.me/chxm1023');
-$done({status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(chxm1023)});
+const url = $request.url;
+const isQX = typeof $task !== "undefined";
+var chxm1023 = JSON.parse($response.body);
+const subscriptionTest = /nicegram\.cloud\/api\/v\d\/user\/info/;
+const premiumTest = /restoreAccess/;
+
+if (subscriptionTest.test(url)) {
+  chxm1023.data.user = {
+    ...chxm1023.data.user,
+    subscription: true,
+    store_subscription: true,
+    lifetime_subscription: true
+  };
+}
+
+if (premiumTest.test(url)) {
+  chxm1023 = {"data":{"premiumAccess": true}};
+}
+
+function finalizeResponse(content) {
+  return { status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(content) };
+}
+
+$done(isQX ? finalizeResponse(chxm1023) : chxm1023);
