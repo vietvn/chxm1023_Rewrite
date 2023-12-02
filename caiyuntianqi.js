093a2a7bfe94420f39d5db3ffd4f2fc9f57f601a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-12-02 18:59:22 +0800

    更新 caiyuntianqi.js

--- caiyuntianqi.js
+++ caiyuntianqi.js
@@ -1,7 +1,9 @@
 /*************************************
 
-项目名称：彩云天气
+项目名称：彩云天气+彩云天气Pro
 下载地址：https://t.cn/A66d95hV
+版本支持：7.6.0
+更新日期：2023-12-02
 脚本作者：chxm1023
 电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
@@ -42,12 +44,12 @@ if (typeof $response == "undefined") {
       chxm1023 = {  ...chxm1023,  vip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  },  trial_svip: {    ...chxm1023.trial_svip,    "received_time" : "1666666666",    "expires_time" : "4092599349",    "is_recharge_vip" : true  },  svip: {    "expires_time" : "4092599349",    "is_auto_renewal" : true  }};
       break;
     case userUrl.test(url):
-      chxm1023.result = { ...chxm1023.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 9999,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...chxm1023.result.wt,  vip: {  ...chxm1023.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 9999,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  phone_num: "13145200000",  name: "叮当猫の分享频道",  avatar: "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132",  bound_status: {  ...chxm1023.result.bound_status,  caiyun: {  ...chxm1023.result.bound_status.caiyun,  "username" : "叮当猫の分享频道",  "is_bound" : true}}};
+      chxm1023.result = { ...chxm1023.result,  is_vip: true,  vip_expired_at: 4092599349,  svip_given: 9999,  is_xy_vip: true,  xy_svip_expire: 4092599349,  wt: {  ...chxm1023.result.wt,  vip: {  ...chxm1023.result.wt.vip,  "expired_at" : 0,  "enabled" : true,  "svip_apple_expired_at" : 4092599349,  "is_auto_renewal" : true,  "svip_expired_at" : 4092599349    },    svip_given: 9999,  },  is_phone_verified: true,  vip_take_effect: 1,  is_primary: true,  xy_vip_expire: 4092599349,  svip_expired_at: 4092599349,  svip_take_effect: 1,  vip_type: "s",  phone_num: "13145200000",  name: "叮当猫の分享频道",  avatar: "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm.png",  bound_status: {  ...chxm1023.result.bound_status,  caiyun: {  ...chxm1023.result.bound_status.caiyun,  "username" : "叮当猫の分享频道",  "is_bound" : true}}};
       break;
     case infoUrl.test(url):
       chxm1023["reg_days"] = 99999;
       chxm1023["name"] = "叮当猫の分享频道";
-      chxm1023["avatar"] = "https://thirdwx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTI6OLWyDbM42w944dUdfzrPEQv6uaD3AEsVcicibYibxG9PBBSaibwqRJzOk2US1d8N4hC9nL1a5rXu3g/132";
+      chxm1023["avatar"] = "https://raw.githubusercontent.com/chxm1023/Script_X/main/icon/ddm.png";
       break;
     }
   chxm1024.body = JSON.stringify(chxm1023);
