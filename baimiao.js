150bda076ace5b7880a8aac87bb9e6855fcdb59e
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2023-12-05 18:01:57 +0800

    Update baimiao.js

--- baimiao.js
+++ baimiao.js
@@ -24,7 +24,7 @@ const ad = /(getAnnouncement|checkLoginClient)/;
 const Data = {"id":102366,"boughtType":"new","createdTime":1555777247,"levelSeq":2,"boughtUnit":"year","levelId":2,"userId":1666666,"boughtTime":1555777247,"boughtDuration":10,"orderId":160888,"operatorId":0,"level":{"isSubscribe":0,"monthPrice":0.02,"maxRate":100,"yearPrice":40,"recognizeNormal":-100,"upgradeSubscribePrice":0,"picture":"","icon":"","gived":0,"recognizeTranslateAll":1,"pdfTranslateCount":0,"name":"黄金会员","createdTime":1429260383,"id":2,"enabled":1,"recognizeBatch":-100,"seq":2,"pdfTransCount":0,"recognizeTranslate":-100,"description":""},"deadlineNotified":0,"deadline":1871396447,"boughtAmount":26};
 
 if(user.test($request.url)){
-  chxm1023.value.recognize = {...chxm1023.value.recognize,"remainBatch":-100,"remainShare":99,"recognizeTranslateAll":1,"translateCount":99,"remainPdfTranslateCount":99,"pdfTranslateCount":99,"remainNormal":-100,"userId":1666666,"pdfCount":99,"remainPdfTransCount":99,"balanceCount":99,"shareCount":99,"remainTranslate":-100,"batchCount":99,"normalCount":-100};
+  chxm1023.value.recognize = {"remainBatch":-100,"remainShare":3,"recognizeTranslateAll":1,"translateCount":3,"remainPdfTranslateCount":0,"pdfTranslateCount":0,"remainNormal":-100,"userId":1666666,"pdfCount":0,"remainPdfTransCount":0,"balanceCount":0,"shareCount":3,"remainTranslate":-100,"batchCount":1,"normalCount":-100};
   chxm1023.value.vips = [(Data)];
   chxm1023.value.vip = (Data);
 }
