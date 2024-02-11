604565d559c7c4bd7ac70ab684cc8f6385e13ba2
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2024-02-11 10:09:58 +0800

    更新 yunting.js

--- yunting.js
+++ yunting.js
@@ -2,14 +2,16 @@
 
 项目名称：云听 全国电台/有声听书
 下载地址：https://t.cn/A6ouQ21g
+版本支持：7.0.35
+更新日期：2024-02-11
 脚本作者：chxm1023
+电报频道：https://t.me/chxm1023
 使用声明：⚠️仅供参考，🈲转载与售卖！
-使用说明：已解锁免费在线听以及可下载。
 
 **************************************
 
 [rewrite_local]
-(^https?:\/\/(ytmsout|ytapi)\.radio\.cn|60\.205\.171\.165)\/(contentBiz|publish|rights|user\/appUser\/getUserInfo|ytsrv\/srv\/appUser\/getUserInfoH5) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/yunting.js
+(^https?:\/\/(ytmsout|ytapi|getway)\.radio\.cn|60\.205\.171\.165)\/(contentBiz|publish|rights|(user|ytsrv\/srv)\/(appUser|webPage)\/getUserInfo|app.+) url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/yunting.js
 
 [mitm]
 hostname = *.radio.cn, 60.205.171.165
@@ -17,17 +19,17 @@ hostname = *.radio.cn, 60.205.171.165
 *************************************/
 
 
-const js1 = '/ytmsout';
-const ad1 = '/publish/recScreen/getLoadPage';
-const ad2 = '/publish';
-const vip1 = '/user/appUser/getUserInfo';
-const vip2 = '/ytsrv/srv/appUser/getUserInfoH5';
-const vip3 = '/rights/userRights/memberCenterRightsDetail';
 var body = $response.body;
 var chxm1023 = JSON.parse(body);
+const js = /(ytmsout|ytapi|getway)/;
+const ad = /publish\/recScreen\/getLoadPage/;
+const user1 = /user\/(appUser|webPage)\/getUserInfo/;
+const user2 = /ytsrv\/srv\/appUser\/getUserInfo/;
+const vip = /rights\/userRights\/memberCenterRightsDetail/;
+const hf = /publish\/layout\/queryLayoutPageByPo/;
 
-//解锁听书&下载内容
-if ($request.url.indexOf(js1) != -1){
+//解锁VIP/付费/下载
+if(js.test($request.url)){
   body = body.replace(/\"needPay":\d+/g, '\"needPay":0');
   body = body.replace(/\"songVirtualPrice":\d+/g,'\"songVirtualPrice":0');
   body = body.replace(/\"songNeedPay":\d+/g, '\"songNeedPay":0');
@@ -42,50 +44,66 @@ if ($request.url.indexOf(js1) != -1){
   body = body.replace(/\"singleDownloadFlag":\w+/g, '\"singleDownloadFlag":true');
   body = body.replace(/\"auditionFlag":\w+/g, '\"auditionFlag":true');
   body = body.replace(/\"auditionFlag":\d+/g, '\"auditionFlag":1');
+  body = body.replace(/\"recBubbleVo":{[^}]*}/g, '\"recBubbleVo":null');
+  body = body.replace(/"banner":\[[^\]]*\]/g, '"banner":[]');
+  body = body.replace(/\"url":".*?"/g, '\"url":null');
 }
 
 //开屏/弹窗处理
-if ($request.url.indexOf(ad1) != -1){
+if(ad.test($request.url)){
   body = body.replace(/{.*?}$/g, '\{}');
 }
 
-//角标处理
-if ($request.url.indexOf(ad2) != -1){
-  body = body.replace(/\"markText":"\w+"/g, '\"markText":""');
-  body = body.replace(/\"contentMarkImg":".*?"/g, '\"contentMarkImg":""');
-}
-
-//VIP信息
-if ($request.url.indexOf(vip1) != -1){
-  chxm1023.data.sex = 1;
-  chxm1023.data.vipExpireTime = 4092599349000;
-  chxm1023.data.vipFlag = 1;
+//用户信息
+if(user1.test($request.url)){
+  chxm1023.data = {
+    ...chxm1023.data,
+    "userName" : "叮当猫",
+    "vipFlag" : 1,
+    "nickName" : "叮当猫",
+    "icon" : "http://yunting-bj-radio-client.oss-cn-beijing.aliyuncs.com/25010%2Fsc_upload%2F202301%2F22%2F09%2F4eL7lc2023012209974.JPEG",
+    "vipExpireTime" : 4092599349000
+  };
   chxm1023.data.memberMarkVo = {
-      "signActiveImg" : "https://ytmedia.radio.cn/CCYT%2F2023%2F01%2F06%2F1672991809upc7cd442706edf9cd097eab02ddbc0fe7.png",
-      "signUnactiveImg" : "https://ytmedia.radio.cn/CCYT%2F2023%2F01%2F06%2F1672992265up413309085e3fc6878f935a74d2ca73b3.png",
-      "foreColorVal" : "#292421",
-      "markText" : "VIP身份",
-      "packageId" : "1002",
-      "fitVersion" : "1",
-      "packageActiveFlag" : true,
-      "packageName" : "VIP",
-      "backGroundColorVal" : "#F0FFFF"
-    };
+    ...chxm1023.data.memberMarkVo,
+    "signActiveImg" : "https://ytmedia.radio.cn/CCYT%2F2023%2F01%2F06%2F1672991809upc7cd442706edf9cd097eab02ddbc0fe7.png",
+    "foreColorVal" : "#292421",
+    "markText" : "VIP身份",
+    "packageId" : "1002",
+    "fitVersion" : "1",
+    "packageActiveFlag" : true,
+    "packageName" : "VIP",
+    "backGroundColorVal" : "#F0FFFF"
+  };
   body = JSON.stringify(chxm1023);
 }
 
-if ($request.url.indexOf(vip2) != -1){
-  chxm1023.object.baseInfo.isVip = 1;
-  chxm1023.object.baseInfo.vipTime = "2099-09-09";
+if(user2.test($request.url)){
+  chxm1023.object.baseInfo = {
+    "isVip" : 1,
+    "vipTime" : "2099-09-09",
+    "nickName" : "叮当猫",
+    "userIcon" : "http://yunting-bj-radio-client.oss-cn-beijing.aliyuncs.com/25010%2Fsc_upload%2F202301%2F22%2F09%2F4eL7lc2023012209974.JPEG"
+  };
   body = JSON.stringify(chxm1023);
 }
 
-if ($request.url.indexOf(vip3) != -1){
+//VIP面板时间
+if(vip.test($request.url)){
   chxm1023.data.userPackageExpireVo =  {
-      "endTime" : "2099-09-09 09:09:09",
-      "startTime" : "2023-07-20 00:00:00"
-    };
+    "endTime" : "2099-09-09 09:09:09",
+    "startTime" : "2023-07-20 00:00:00"
+  };
+  body = JSON.stringify(chxm1023);
+}
 
+//处理横幅
+if(hf.test($request.url)){
+  chxm1023.data.data = chxm1023.data.data.filter(item => {
+    const isTitleMismatch = item.plateVo && item.plateVo.plateContentList &&  !item.plateVo.plateContentList.some(  content => content.title === "一分钟看懂VIP vs 付费"  );
+    const isLayoutMismatch = item.layoutName && item.layoutName !== "VIP频道banner（16:9）" &&  item.layoutName !== "频道banner-VIP" &&  item.layoutName !== "Banner（16:9）" &&  item.layoutName !== "banner";
+    return isTitleMismatch && isLayoutMismatch;
+  });
   body = JSON.stringify(chxm1023);
 }
 
