6e9d5eecab35c467e0fe05bf839adead58d6436a
chxm1023 <106084065+chxm1023@users.noreply.github.com>
2024-03-08 02:31:12 +0800

    更新 filmix.js

--- filmix.js
+++ filmix.js
@@ -12,7 +12,6 @@
 [rewrite_local]
 ^https?:\/\/appv2\.filmix\.com\.cn\/api\/v\d\/users url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/filmix.js
 
-
 [mitm]
 hostname = appv2.filmix.com.cn
 
